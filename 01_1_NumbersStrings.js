d52c411eea8eef2c7089eabeb0cee1b2dbdd8eef
James_McNeill <jmcneill06@outlook.com>
2021-07-24 12:21:46 +0000

    Added additional learning sessions

---
+++ 01_1_NumbersStrings.js
@@ -0,0 +1,23 @@
+// Types
+
+// Number
+console.log(10);
+console.log(typeof 10);
+console.log(typeof "10");
+console.log(10 + 1);
+console.log(10 * 2);
+// // Can bring in the built in packages
+console.log(Math.random());
+console.log(Math.PI);
+// Trying to add a string to number results in concatenation
+console.log("10" + 213);
+// Correcting to a integer
+console.log(parseInt("10") + 213);
+console.log(+"10"); // convert string to a number
+console.log(parseInt("10", 10)); // Adding a base to it as an optional argument
+console.log(parseInt("Hello")); // NaN returned as not a number
+console.log(isNaN("hello")); // Check is Not a Number
+
+// Strings
+// Sequence of Unicode characters
+console.log("Hello");
---
+++ 01_3_Objects.js
@@ -0,0 +1,99 @@
+// Objects
+// Collections of name-value pairs
+
+var myName = {1: "James"};
+console.log(myName);
+
+// One method to create object
+var myObject = new Object();
+myObject['1'] = "James";
+myObject['2'] = "Sally";
+myObject['3'] = "Bob";
+console.log(myObject);
+
+// Object literal syntax, preffered
+var myOtherObject = {
+  1: "James"
+  ,2: "Sally"
+  ,3: "Bob"
+  ,4: "Billy"
+  ,5: "Jane"
+}; 
+
+myOtherObject['6'] = "Aisling";
+
+// Dictionary entry - remember to keep keys unique
+var anotherObject = {
+  firstName: "James"
+  ,lastName: "McNeill"
+  ,age: 31
+  ,numbers: {
+    mobile: "000123"
+    ,home: "555555"
+  }
+  ,address: "123 Fake Street"  
+};
+// Added a nested object (numbers)
+
+var userMobileNumber = anotherObject.numbers.mobile;
+
+console.log(anotherObject);
+console.log(anotherObject.firstName);
+console.log(anotherObject['firstName']);
+console.log(anotherObject.numbers);
+console.log(anotherObject.numbers.mobile);
+console.log(anotherObject.numbers.home);
+
+console.log(userMobileNumber);
+
+var donut = {
+  type: "coconut"
+  ,glazed: true
+  ,sweetness: 8
+  ,hasChocolate: false
+  ,sayHi: function(){
+    console.log("Hi");
+  }
+  ,sayType: function() {
+    console.log("Type: " + this.type);
+  }
+  ,showSweetness: function() {
+    console.log("Sweetness: " + this.sweetness + "/10");
+  }
+};
+
+console.log(donut)
+
+// Can also plug a function into an object
+
+// Create simple function
+function sayHi() {
+  console.log("Say Hi!")
+};
+
+sayHi();
+
+// Using the functions from an object
+donut.sayHi();
+donut.sayType();
+donut.showSweetness();
+
+// Constructor pattern for creating objects - boilerplat code
+function Donut(type, glazed, sweetness, hasChocolate) {
+  this.type = type;
+  this.glazed = glazed;
+  this.sweetness = sweetness;
+  this.hasChocolate = hasChocolate;
+  this.sayType = function() {
+    console.log("Type: " + this.type);
+  };
+  this.showSweetness = function() {
+    console.log("Sweetness: " + this.sweetness + "/10");
+  };
+};
+
+// Instantiating the function
+var coconutDonut = new Donut("coconut", false, 8, true);
+// console.log(coconutDonut);
+var vanillaDonut = new Donut("vanilla", true, 10, false);
+vanillaDonut.showSweetness();
